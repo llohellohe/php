54597a690852c464592e45b32d26ea69cf5c6355
llohellohe <yangqichris@gmail.com>
2012-12-22 21:04:49 +0800

    add basic

---
+++ basic.php
@@ -0,0 +1,25 @@
+<?php
+$var=TRUE;
+echo gettype($var)."\n";
+$var2=true;
+echo gettype($var2)."\n";
+$var3=12;
+echo gettype($var3)."\n";
+$var3=12.0;
+echo gettype($var3)."\n";
+$var3="12";
+echo gettype($var3)."\n";
+var_dump($var3);
+
+echo (int)(1.0+2.1);
+
+
+echo "ni
+ho
+a";
+
+echo "\nhello ".$var3;
+echo '\nhello $var3'
+
+
+?>
